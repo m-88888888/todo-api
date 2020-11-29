5994c5e1c68e15f9967515d58fa06b8098cf6f79
m-88888888 <mochi.h8888@gmail.com>
2020-11-29 23:56:32 +0900

    CRUD実装

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -11,16 +14,23 @@ import (
 )
 
 type Task struct {
-	gorm.Model
+	ID      int64 `gorm:"primaryKey"`
 	Name    string
 	Remarks string
 }
 
 func initDB() (db *gorm.DB) {
-	var err error
+	err := godotenv.Load()
+	if err != nil {
+		// .env読めなかった場合の処理
+		log.Fatal("env file loading error")
+	}
+
 	DNS := os.Getenv("NAME") + ":" + os.Getenv("PASS") + "@" + os.Getenv("PROTOCOL") + "/" + os.Getenv("DB_NAME") + "?charset=utf8&parseTime=True&loc=Local"
 
 	db, err = gorm.Open(mysql.Open(DNS), &gorm.Config{})
+
+	db.AutoMigrate(&Task{})
 
 	if err != nil {
 		panic("failed to connect database")
@@ -29,29 +39,72 @@ func initDB() (db *gorm.DB) {
 	return
 }
 
-func returnTask() (task Task) {
-	err := godotenv.Load()
-	if err != nil {
-		// .env読めなかった場合の処理
-		log.Fatal("env file loading error")
-	}
-
+func getTask(w http.ResponseWriter, r *http.Request) {
 	db := initDB()
 
-	// db.AutoMigrate(&Task{})
-
-	// task := Task{
-	// 	Name:    "evans試す",
-	// 	Remarks: "https://github.com/ktr0731/evans",
-	// }
-
-	// db.Create(&task)
-
-	db.First(&task, 1)
+	id := r.FormValue("id")
+	var task Task
+	db.First(&task, "id = ?", id)
 	fmt.Println(task)
-	return
+	json.NewEncoder(w).Encode(task)
+}
+
+func getAllTasks(w http.ResponseWriter, r *http.Request) {
+	db := initDB()
+
+	var tasks []Task
+	db.Find(&tasks)
+	fmt.Println(tasks)
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func insertTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	task.Name = r.FormValue("name")
+	task.Remarks = r.FormValue("remarks")
+	fmt.Println(task)
+
+	db := initDB()
+	result := db.Create(&task)
+	if result.RowsAffected > 0 {
+		fmt.Println("task insert success")
+	} else {
+		log.Fatal("Failed to insert task")
+	}
+
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	var task Task
+	task.ID, _ = strconv.ParseInt(id, 10, 64)
+	fmt.Println(task)
+
+	db := initDB()
+	db.Debug().Delete(&task)
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	var task Task
+	task.ID, _ = strconv.ParseInt(id, 10, 64)
+	task.Name = r.FormValue("name")
+	task.Remarks = r.FormValue("remarks")
+
+	db := initDB()
+	db.Debug().Model(&task).Where("id = ?", id).Updates(task)
+}
+
+func handleRequests() {
+	// ハンドラ。URLパスと関数を結びつける
+	http.HandleFunc("/task", getTask)
+	http.HandleFunc("/tasks", getAllTasks)
+	http.HandleFunc("/insert", insertTask)
+	http.HandleFunc("/delete", deleteTask)
+	http.HandleFunc("/update", updateTask)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func main() {
-	returnTask()
+	handleRequests()
 }
